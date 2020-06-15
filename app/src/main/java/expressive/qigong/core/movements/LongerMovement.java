fd3dd5b703f7d600dd994d229ff429ce1a260fc5
gabrielecuccu <gabriele.cuccu@gmail.com>
2020-06-15 15:42:06 +0100

    improved duration of movements

--- app/src/main/java/expressive/qigong/core/movements/LongerMovement.java
+++ app/src/main/java/expressive/qigong/core/movements/LongerMovement.java
@@ -2,10 +2,10 @@ package expressive.qigong.core.movements;
 
 public class LongerMovement extends Movement {
     public LongerMovement(String name, String set) {
-        super(name, set, "", 2);
+        super(name, set, "", 1.5f);
     }
 
     public LongerMovement(String name, String set, String hint) {
-        super(name, set, hint, 2);
+        super(name, set, hint, 1.5f);
     }
 }
