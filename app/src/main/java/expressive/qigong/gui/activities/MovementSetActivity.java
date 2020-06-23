732767f5e8a6b7624e2e79a9b4a25d955cf9d917
gabrielecuccu <gabriele.cuccu@gmail.com>
2020-06-23 14:03:15 +0100

    new music added

--- app/src/main/java/expressive/qigong/gui/activities/MovementSetActivity.java
+++ app/src/main/java/expressive/qigong/gui/activities/MovementSetActivity.java
@@ -1,37 +1,56 @@
 package expressive.qigong.gui.activities;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+import static android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
-import androidx.preference.PreferenceManager;
+import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
 import expressive.qigong.R;
 import expressive.qigong.gui.controllers.MovementSetActivityControllerImpl;
 import expressive.qigong.gui.core.MovementSetActivityController;
 import expressive.qigong.gui.core.MovementSetActivityView;
 
-public class MovementSetActivity extends Activity implements TextToSpeech.OnInitListener, MovementSetActivityView {
+public class MovementSetActivity extends Activity
+        implements TextToSpeech.OnInitListener, MovementSetActivityView {
 
     private TextToSpeech tts;
 
-    private final MovementSetActivityController controller = new MovementSetActivityControllerImpl(this);
+    private final MovementSetActivityController controller =
+            new MovementSetActivityControllerImpl(this);
 
     private MediaPlayer mediaPlayer;
+
+    private List<Integer> musicIds = Arrays.asList(
+            R.raw.india,
+            R.raw.littleplanet,
+            R.raw.relaxing,
+            R.raw.deepblue,
+            R.raw.love,
+            R.raw.november,
+            R.raw.slowmotion);
+
+    private int currentMusicIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_movement_set);
 
         controller.processIntentExtra(getIntent().getExtras().getString("set"));
@@ -40,7 +59,8 @@ public class MovementSetActivity extends Activity implements TextToSpeech.OnInit
         findViewById(R.id.back_button).setOnClickListener(view -> controller.backButtonPressed());
         findViewById(R.id.speak_button).setOnClickListener(view -> controller.speakButtonClicked());
 
-        this.tts = new TextToSpeech(this, this);
+        tts = new TextToSpeech(this, this);
+        Collections.shuffle(musicIds);
     }
 
     @Override
@@ -133,79 +153,92 @@ public class MovementSetActivity extends Activity implements TextToSpeech.OnInit
 
     @Override
     public boolean getIncludeWarmUp() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_WARM_UP_TO_RANDOM", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_WARM_UP_TO_RANDOM", false);
     }
 
     @Override
     public boolean getIncludeFiveGates() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_FIVE_GATES_TO_RANDOM", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_FIVE_GATES_TO_RANDOM", false);
     }
 
     @Override
     public boolean getIncludeFiveElements() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_FIVE_ELEMENTS_TO_RANDOM", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_FIVE_ELEMENTS_TO_RANDOM", false);
     }
 
     @Override
     public boolean getIncludeCoiling() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_COILING_TO_RANDOM", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_COILING_TO_RANDOM", false);
     }
 
     @Override
     public boolean getIncludeBlackDragon() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_BLACK_DRAGON_TO_RANDOM", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_BLACK_DRAGON_TO_RANDOM", false);
     }
 
     @Override
     public int getMovementDuration() {
-        String duration = PreferenceManager.getDefaultSharedPreferences(this).getString("RANDOM_DURATION", "90");
+        String duration = getDefaultSharedPreferences(this).getString("RANDOM_DURATION", "90");
         return Integer.parseInt(duration);
     }
 
     @Override
     public boolean getSuggestTenPrinciples() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("SUGGEST_TEN_PRINCIPLES", true);
+        return getDefaultSharedPreferences(this).getBoolean("SUGGEST_TEN_PRINCIPLES", true);
     }
 
     @Override
     public int getNumberOfMovements() {
-        String number = PreferenceManager.getDefaultSharedPreferences(this).getString("RANDOM_NUMBER_OF_MOVEMENTS", "10");
+        String number = getDefaultSharedPreferences(this).getString("RANDOM_NUMBER_OF_MOVEMENTS", "10");
         return Integer.parseInt(number);
     }
 
     @Override
     public boolean getAddWarmUpToAllSets() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("ADD_WARM_UP_TO_ALL_SETS", false);
+        return getDefaultSharedPreferences(this).getBoolean("ADD_WARM_UP_TO_ALL_SETS", false);
     }
 
     @Override
     public void keepScreenOn() {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(FLAG_KEEP_SCREEN_ON);
     }
 
     @Override
     public void clearKeepScreenOn() {
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().clearFlags(FLAG_KEEP_SCREEN_ON);
     }
 
     @Override
     public void playMusic() {
-        final boolean shouldPlayMusic =
-                PreferenceManager.getDefaultSharedPreferences(this).getBoolean("PLAY_MUSIC", true);
+        boolean shouldPlayMusic = getDefaultSharedPreferences(this).getBoolean("PLAY_MUSIC", true);
 
-        if (this.mediaPlayer == null && shouldPlayMusic) {
-            this.mediaPlayer = MediaPlayer.create(this, R.raw.relaxing);
-            this.mediaPlayer.setVolume(0.5f, 0.5f);
-            this.mediaPlayer.start();
+        if (mediaPlayer == null && shouldPlayMusic) {
+            mediaPlayer = MediaPlayer.create(this, musicIds.get(currentMusicIndex));
+            mediaPlayer.setVolume(0.75f, 0.75f);
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(mp -> {
+                currentMusicIndex = (currentMusicIndex + 1) % musicIds.size();
+                AssetFileDescriptor afd =
+                        getResources().openRawResourceFd(musicIds.get(currentMusicIndex));
+                try {
+                    mp.reset();
+                    mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                    afd.close();
+                    mp.prepare();
+                    mp.start();
+                } catch (IOException ignored) {
+
+                }
+            });
         }
     }
 
     @Override
     public void stopMusic() {
-        if (this.mediaPlayer != null) {
-            this.mediaPlayer.stop();
-            this.mediaPlayer.release();
-            this.mediaPlayer = null;
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
 }
